54b2a7f5229cfae89b7ddee720086b90863916da
mohamed <mnaguib126@gmail.com>
2022-01-24 15:55:43 +0200

    all admin-lte and multi lang

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.dashboard.app')
+@section('content')
+    <div class="content-wrapper">0</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Dashboard\DachboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => LaravelLocalization::setLocale(),'middleware'=> ['localizationRedirect', 'localeSessionRedirect', 'localeViewPath']], function(){
+    Route::group(['prefix' => 'dashboard'], function(){
+        Route::get('welcome',  [DachboardController::class,'index'])->name('dashboard.index');
+    });
+});
+
 
 Auth::routes();
 
